f367be362691e5315abb421e51a5566a261c6d99
LeoSouzaSenac <ledsouza@senacrs.com.br>
2026-07-07 13:46:52 -0300

    exemplo

---
+++ 01-07/src/utils/omitPassword.ts
@@ -0,0 +1,12 @@
+// esta função servirá para remover o campo de senha (password) de um objeto User
+// Isso vai fazer com que, quando chamarmos ela em Services, ele envia ao banco o usuário normal (completo) mas envia para o Controller um usuário que não tem senha, assim o JSON não contém a senha do usuário
+
+import { User } from "../models/User";
+
+export function omitPassword(user:User){
+    // copiamos o valor da senha do user para a variavel password
+    // ai, o resto (id, name, email) fica dentro da variavel rest
+    // e é ela que retornamos
+    const { password, ...rest} = user
+    return rest
+}
